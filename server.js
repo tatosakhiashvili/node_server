d095cdf12f2d669173630bfa6a88468fe7d911c1
Tato Sakhiashvili <besiksakhiashvili@Besiks-MacBook-Pro.local>
2025-01-26 00:59:10 +0400

    asdasdasd

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+server.use(cors()); // Enable CORS
 server.use(middlewares);
 server.use(router);
+
 server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running');
 });
